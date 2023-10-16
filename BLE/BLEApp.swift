a28ec9ceca8c7c6fa40147d3292a5702f837ee9c
Christian <christian00kh@gmail.com>
2023-10-16 13:56:17 +0200

    Initial Commit

---
+++ BLE/BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEApp.swift
+//  BLE
+//
+//  Created by Simon Khederchah on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BLE
+//
+//  Created by Simon Khederchah on 2023-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
